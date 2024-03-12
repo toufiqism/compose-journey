2d0c481d24f784decb1939a1385fe0915a42700c
Shawon :) <toufiqakbar@gmail.com>
2024-03-12 23:00:20 +0600

    Search bar ui added

--- PokedexApp/app/src/main/java/com/toufiq/pokedexapp/ui/screens/PokemonListScreen.kt
+++ PokedexApp/app/src/main/java/com/toufiq/pokedexapp/ui/screens/PokemonListScreen.kt
@@ -2,19 +2,28 @@ package com.toufiq.pokedexapp.ui.screens
 
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -25,7 +34,7 @@ import com.toufiq.pokedexapp.R
 @Composable
 fun PokemonListScreen(navController: NavController) {
 
-    Surface(color = MaterialTheme.colorScheme.background, modifier = Modifier.fillMaxSize()) {
+    Surface(color = Color.Gray, modifier = Modifier.fillMaxSize()) {
         Column {
             Spacer(modifier = Modifier.height(20.dp))
             Image(
@@ -35,6 +44,13 @@ fun PokemonListScreen(navController: NavController) {
                     .fillMaxWidth()
                     .align(Alignment.CenterHorizontally)
             )
+            SearchBar(
+                hintText = "Search", modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+
+            }
 
         }
 
@@ -49,5 +65,45 @@ fun PokemonListScreenPreview() {
 }
 
 @Composable
-fun SearchBar(modifier: Modifier = Modifier, hintText: String) {
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    hintText: String = "",
+    onSearch: (String) -> Unit = {}
+) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    var isHintDisplayed by remember {
+        mutableStateOf(hintText != "")
+    }
+    Box(modifier = modifier) {
+        BasicTextField(
+            value = text,
+            onValueChange = {
+                text = it
+                onSearch(it)
+            },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(
+                    5.dp,
+                    CircleShape
+                )
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged {
+                    isHintDisplayed = it.isFocused != true
+                }
+        )
+        if (isHintDisplayed) {
+            Text(
+                text = hintText,
+                color = Color.LightGray,
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)
+            )
+        }
+    }
 }
