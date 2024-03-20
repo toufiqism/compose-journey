1a890dc690d5dcf7a04f5050ae15a528bb09489e
Shawon :) <toufiqakbar@gmail.com>
2024-03-21 03:01:51 +0600

    Pokemon details screen wip

--- PokedexApp/app/src/main/java/com/toufiq/pokedexapp/ui/PokemonDetailViewModel.kt
+++ PokedexApp/app/src/main/java/com/toufiq/pokedexapp/ui/PokemonDetailViewModel.kt
@@ -1,11 +1,17 @@
 package com.toufiq.pokedexapp.ui
 
 import androidx.lifecycle.ViewModel
+import com.toufiq.pokedexapp.data.remote.responses.Pokemon
 import com.toufiq.pokedexapp.repository.PokemonRepository
+import com.toufiq.pokedexapp.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonDetailViewModel @Inject constructor(private val repository: PokemonRepository) :
     ViewModel() {
+
+        suspend fun getPokemonInfo(pokemonName:String):Resource<Pokemon>{
+            return repository.getPokemonInfo(pokemonName)
+        }
 }
--- PokedexApp/app/src/main/java/com/toufiq/pokedexapp/ui/screens/PokemonDetailsScreen.kt
+++ PokedexApp/app/src/main/java/com/toufiq/pokedexapp/ui/screens/PokemonDetailsScreen.kt
@@ -1,7 +1,58 @@
 package com.toufiq.pokedexapp.ui.screens
 
+import androidx.compose.animation.fadeIn
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.toufiq.pokedexapp.data.remote.responses.Pokemon
+import com.toufiq.pokedexapp.ui.PokemonDetailViewModel
+import com.toufiq.pokedexapp.util.Resource
 
 @Composable
-fun PokemonDetailsScreen (){
+fun PokemonDetailsScreen(
+    pokemonName: String,
+    navController: NavController,
+    topPadding: Dp = 20.dp,
+    pokemonImageSize: Dp = 200.dp,
+    viewModel: PokemonDetailViewModel = hiltViewModel()
+) {
+    val pokemonInfo =
+        produceState<Resource<Pokemon>>(initialValue = Resource.Loading()) {
+            value = viewModel.getPokemonInfo(pokemonName)
+        }.value
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 16.dp)
+    ) {
+        Box(contentAlignment = Alignment.TopCenter, modifier = Modifier.fillMaxSize()) {
+            if (pokemonInfo is Resource.Success) {
+                pokemonInfo.data?.sprites?.let {
+                    AsyncImage(
+                        model = it.frontDefault,
+                        contentDescription = pokemonInfo.data.name,
+                        modifier = Modifier
+                            .size(pokemonImageSize)
+                            .offset(y = topPadding)
+                    )
+                }
+            }
+        }
+    }
 }
+@Composable
+fun PokemonDetailTopSection(navController: NavController,modifier: Modifier=Modifier){}
