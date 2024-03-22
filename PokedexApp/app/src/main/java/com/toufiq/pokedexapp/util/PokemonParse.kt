6911b209b9f835b10db5173aefe8fb45491a60a6
Shawon :) <toufiqakbar@gmail.com>
2024-03-22 21:39:24 +0600

    Pokemon details screen weight and height data shown in details section

---
+++ PokedexApp/app/src/main/java/com/toufiq/pokedexapp/util/PokemonParse.kt
@@ -0,0 +1,57 @@
+package com.toufiq.pokedexapp.util
+
+import androidx.compose.ui.graphics.Color
+import com.toufiq.pokedexapp.data.remote.responses.Stat
+import com.toufiq.pokedexapp.ui.theme.*
+import com.toufiq.pokedexapp.data.remote.responses.Type
+import com.toufiq.pokedexapp.ui.theme.TypeFire
+import com.toufiq.pokedexapp.ui.theme.TypeNormal
+import java.util.*
+
+fun parseTypeToColor(type: Type): Color {
+    return when (type.type!!.name!!.lowercase(Locale.ROOT)) {
+        "normal" -> TypeNormal
+        "fire" -> TypeFire
+        "water" -> TypeWater
+        "electric" -> TypeElectric
+        "grass" -> TypeGrass
+        "ice" -> TypeIce
+        "fighting" -> TypeFighting
+        "poison" -> TypePoison
+        "ground" -> TypeGround
+        "flying" -> TypeFlying
+        "psychic" -> TypePsychic
+        "bug" -> TypeBug
+        "rock" -> TypeRock
+        "ghost" -> TypeGhost
+        "dragon" -> TypeDragon
+        "dark" -> TypeDark
+        "steel" -> TypeSteel
+        "fairy" -> TypeFairy
+        else -> Color.Black
+    }
+}
+
+fun parseStatToColor(stat: Stat): Color {
+    return when (stat.stat!!.name!!.lowercase(Locale.ROOT)) {
+        "hp" -> HPColor
+        "attack" -> AtkColor
+        "defense" -> DefColor
+        "special-attack" -> SpAtkColor
+        "special-defense" -> SpDefColor
+        "speed" -> SpdColor
+        else -> Color.White
+    }
+}
+
+fun parseStatToAbbr(stat: Stat): String {
+    return when (stat.stat!!.name!!.lowercase(Locale.ROOT)) {
+        "hp" -> "HP"
+        "attack" -> "Atk"
+        "defense" -> "Def"
+        "special-attack" -> "SpAtk"
+        "special-defense" -> "SpDef"
+        "speed" -> "Spd"
+        else -> ""
+    }
+}
