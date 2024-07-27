b560308dc1f946747741b4f0b1fe9f76157d94ec
Toufiq Akbar <toufiqakbar@gmail.com>
2024-07-27 18:59:31 +0600

    added dice project

--- ComposeStateExample/app/src/main/java/com/toufiq/composestateexample/CounterApp.kt
+++ ComposeStateExample/app/src/main/java/com/toufiq/composestateexample/CounterApp.kt
@@ -5,9 +5,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,14 +16,14 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun CounterApp(modifier: Modifier = Modifier) {
-    val count = remember {
-        mutableIntStateOf(0)
+    var count by remember {
+        mutableStateOf(0)
     }
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "${count.intValue}", fontSize = 80.sp)
+        Text(text = "$count", fontSize = 80.sp)
         Button(onClick = {
-            count.intValue++
-            Log.d("TAG", "CounterApp: ${count.intValue}")
+            count++
+            Log.d("TAG", "CounterApp: $count")
         }) {
             Text(text = "Click ME!")
         }
